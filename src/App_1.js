7aaf9c219b04c171398efe117414b45ef500dcaa
Bivek Shrestha <bivekcesta@gmail.com>
2021-02-11 19:37:42 -0600

    added homepage route as default

--- src/App_1.js
+++ src/App_1.js
@@ -1,52 +1,41 @@
-import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
-import AdminLayout from "./layouts/Admin";
-import AuthLayout from "./layouts/Auth";
-import React, {Component} from "react";
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import AdminLayout from './layouts/Admin';
+import AuthLayout from './layouts/Auth';
+import React, { Component } from 'react';
 import Alert from 'react-s-alert';
-import LoadingIndicator from "./common/LoadingIndicator";
-
-
-
-
+import LoadingIndicator from './common/LoadingIndicator';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            authenticated: false,
-        }
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			authenticated: false,
+		};
+	}
 
-    componentDidMount() {
-        
-    }
+	componentDidMount() {}
 
+	render() {
+		if (this.state.loading) {
+			return <LoadingIndicator />;
+		}
 
-
-    render() {
-            if(this.state.loading) {
-                return <LoadingIndicator />
-            }
-
-            return (
-                <div className="app">
-                    {/*<div className="app-top-box">
+		return (
+			<div className='app'>
+				{/*<div className="app-top-box">
                         <AppHeader authenticated={this.state.authenticated} onLogout={this.handleLogout} />
                     </div>*/}
-                    <div className="app-body">
-                        <Switch>
-                            <Route path="/admin" render={props => <AdminLayout {...props} />} />
-                            <Route path="/auth" render={props => <AuthLayout {...props} />} />
-                            <Redirect from="/" to="/admin/blog/all" />
-                        </Switch>
-                    </div>
-                    <Alert stack={{limit: 3}}
-                           timeout = {3000}
-                           position='top-right' effect='slide' offset={65} />
-                </div>
-            );
-        }
-    }
+				<div className='app-body'>
+					<Switch>
+						<Route path='/admin' render={(props) => <AdminLayout {...props} />} />
+						<Route path='/auth' render={(props) => <AuthLayout {...props} />} />
+						<Redirect from='/' to='/admin/home' />
+					</Switch>
+				</div>
+				<Alert stack={{ limit: 3 }} timeout={3000} position='top-right' effect='slide' offset={65} />
+			</div>
+		);
+	}
+}
 
-    export default App;
-
+export default App;
--- src/routes.js
+++ src/routes.js
@@ -25,85 +25,90 @@ import Icons from 'views/examples/Icons.js';
 import Blog from 'views/blogs/Blog.js';
 import CreateBlog from 'views/blogs/CreateBlog.js';
 import bmr from 'views/bmr/bmr';
+import home from 'views/homepage';
 import BlogDetail from 'views/blogs/BlogDetail';
 var routes = [
-  {
-    path: "/index",
-    name: "Dashboard",
-    icon: "ni ni-tv-2 text-primary",
-    component: Index,
-    layout: "/admin"
-  },
-  {
-    path: "/icons",
-    name: "Icons",
-    icon: "ni ni-planet text-blue",
-    component: Icons,
-    layout: "/admin"
-  },
-  {
-    path: "/maps",
-    name: "Maps",
-    icon: "ni ni-pin-3 text-orange",
-    component: Maps,
-    layout: "/admin"
-  },
-  {
-    path: "/user-profile",
-    name: "User Profile",
-    icon: "ni ni-single-02 text-yellow",
-    component: Profile,
-    layout: "/admin"
-  },
-  {
-    path: "/tables",
-    name: "Tables",
-    icon: "ni ni-bullet-list-67 text-red",
-    component: Tables,
-    layout: "/admin"
-  },
-  {
-    path: "/login",
-    name: "Login",
-    icon: "ni ni-key-25 text-info",
-    component: Login,
-    layout: "/auth"
-  },
-  {
-    path: "/register",
-    name: "Register",
-    icon: "ni ni-circle-08 text-pink",
-    component: Register,
-    layout: "/auth"
-  },
-  {
-    path: "/blog/all",
-    name: "Blog",
-    icon: "ni ni-books",
-    component: Blog,
-    layout: "/admin"
-  },
-  {
-    path: "/blog/new",
-    name: "CreateBlog",
-    icon: "ni ni-books",
-    component: CreateBlog,
-    layout: "/admin"
-  },
-  {
-    path: "/blog/detail",
-    name: "BlogDetail",
-    icon: "ni ni-books",
-    component: BlogDetail,
-    layout: "/admin"
-  },
-  {
-    path: "/bmr",
-    name: "BMR Calculator",
-    component: bmr,
-    layout: "/admin"
-  }
-
-
+	{
+		path: '/index',
+		name: 'Dashboard',
+		icon: 'ni ni-tv-2 text-primary',
+		component: Index,
+		layout: '/admin',
+	},
+	{
+		path: '/icons',
+		name: 'Icons',
+		icon: 'ni ni-planet text-blue',
+		component: Icons,
+		layout: '/admin',
+	},
+	{
+		path: '/maps',
+		name: 'Maps',
+		icon: 'ni ni-pin-3 text-orange',
+		component: Maps,
+		layout: '/admin',
+	},
+	{
+		path: '/user-profile',
+		name: 'User Profile',
+		icon: 'ni ni-single-02 text-yellow',
+		component: Profile,
+		layout: '/admin',
+	},
+	{
+		path: '/tables',
+		name: 'Tables',
+		icon: 'ni ni-bullet-list-67 text-red',
+		component: Tables,
+		layout: '/admin',
+	},
+	{
+		path: '/login',
+		name: 'Login',
+		icon: 'ni ni-key-25 text-info',
+		component: Login,
+		layout: '/auth',
+	},
+	{
+		path: '/register',
+		name: 'Register',
+		icon: 'ni ni-circle-08 text-pink',
+		component: Register,
+		layout: '/auth',
+	},
+	{
+		path: '/blog/all',
+		name: 'Blog',
+		icon: 'ni ni-books',
+		component: Blog,
+		layout: '/admin',
+	},
+	{
+		path: '/blog/new',
+		name: 'CreateBlog',
+		icon: 'ni ni-books',
+		component: CreateBlog,
+		layout: '/admin',
+	},
+	{
+		path: '/blog/detail',
+		name: 'BlogDetail',
+		icon: 'ni ni-books',
+		component: BlogDetail,
+		layout: '/admin',
+	},
+	{
+		path: '/bmr',
+		name: 'BMR Calculator',
+		component: bmr,
+		layout: '/admin',
+	},
+	{
+		path: '/home',
+		name: 'Homepage',
+		component: home,
+		layout: '/admin',
+	},
 ];
 export default routes;
